8c6a7fe227ea89903a946ba73c9cfdecbaf1e247
tudawei <T15162919530@163.com>
2025-05-20 17:02:13 +0800

    init

---
+++ src/main/java/com/hf/netty/NettyPlatApplication.java
@@ -0,0 +1,13 @@
+package com.hf.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyPlatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyPlatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hf/netty/NettyPlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hf.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyPlatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
