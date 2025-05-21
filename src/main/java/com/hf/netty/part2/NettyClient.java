0f21689e982db02804d8657b0494ab6ac5a613b8
tudawei <T15162919530@163.com>
2025-05-21 16:27:39 +0800

    任务队列中的 Task 有 3 种典型使用场景 1.用户程序自定义的普通任务 2.用户自定义定时任务 3.非当前 Reactor 线程调用 Channel 的各种方法 例如在推送系统的业务线程里面，根据用户的标识，找到对应的 Channel 引用，然后调用 Write 类方法向该用户推送消息，就会进入到这种场景。最终的 Write 会提交到任务队列中后被异步消费

---
+++ src/main/java/com/hf/netty/part2/NettyClient.java
@@ -0,0 +1,43 @@
+package com.hf.netty.part2;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author tdw
+ * @date 2025.5.21
+ */
+public class NettyClient {
+
+    public static void main(String[] args) throws Exception{
+        // 客户端需要一个事件循环组
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            // 客户端启动对象
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new NettyClientHandler());// 加入自己的处理器
+                        }
+                    });
+            System.out.println("客户端 ok..");
+            //启动客户端去连接服务器端
+            //关于 ChannelFuture 要分析，涉及到netty的异步模型
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6668).sync();
+            //给关闭通道进行监听
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+
+    }
+
+}
