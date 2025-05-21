91b669f758769e63657c26921a9cd2eeae1dca1d
tudawei <T15162919530@163.com>
2025-05-21 16:45:21 +0800

    异步模型，Future-Listener 机制

---
+++ src/main/java/com/hf/netty/part3/NettyServer.java
@@ -0,0 +1,87 @@
+package com.hf.netty.part3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author tdw
+ * @date 2025.5.21
+ */
+public class NettyServer {
+
+    public static void main(String[] args) throws Exception  {
+
+        /**
+         * 创建两个线程组 bossGroup 和 workerGroup
+         * bossGroup：处理连接请求，获取socketChannel，包装为NioSocketChannel，并将其注册到某个worker EventLoop的Selector上
+         * workerGroup：处理真正的和客户端的业务
+         *  bossGroup 和 workerGroup 含有的子线程(NioEventLoop)的个数，默认实际 cpu核数 * 2
+         */
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup(2);
+        try {
+            // 服务器启动对象，用来配置参数
+            ServerBootstrap bootstrap  = new ServerBootstrap();
+
+            //使用链式编程来进行设置
+            bootstrap.group(bossGroup, workerGroup) //设置两个线程组
+                    .channel(NioServerSocketChannel.class) //使用NioSocketChannel 作为服务器的通道实现
+                    .option(ChannelOption.SO_BACKLOG, 128) // 设置线程队列得到连接个数
+                    .childOption(ChannelOption.SO_KEEPALIVE, true) //设置保持活动连接状态
+                    //          .handler(null) // 该 handler对应 bossGroup , childHandler 对应 workerGroup
+                    .childHandler(new ChannelInitializer<SocketChannel>() {//创建一个通道初始化对象(匿名对象)
+                        //给pipeline 设置处理器
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            //可以使用一个集合管理 SocketChannel， 再推送消息时，可以将业务加入到各个channel 对应的 NIOEventLoop 的 taskQueue 或者 scheduleTaskQueue
+                            System.out.println("客户端建立连接：" + ch.remoteAddress() + "_" +ch.hashCode());
+                            ch.pipeline().addLast(new NettyServerHandler());
+                        }
+                    }); // 给我们的workerGroup 的 EventLoop 对应的管道设置处理器
+
+            System.out.println(".....服务器 is ready...");
+
+            //绑定一个端口并且同步, 生成了一个 ChannelFuture 对象
+            //启动服务器(并绑定端口) sync:主要作用是阻塞当前线程，直到异步操作完成
+            ChannelFuture cf = bootstrap.bind(6668).sync();
+
+            /**
+             * 异步模型
+             * 1.异步的概念和同步相对。当一个异步过程调用发出后，调用者不能立刻得到结果。实际处理这个调用的组件在完成后，通过状态、通知和回调来通知调用者。
+             * 2.Netty 中的 I/O 操作是异步的，包括 Bind、Write、Connect 等操作会简单的返回一个 ChannelFuture。
+             * 3.调用者并不能立刻获得结果，而是通过 Future-Listener 机制，用户可以方便的主动获取或者通过通知机制获得 IO 操作结果
+             * 4.Netty 的异步模型是建立在 future 和 callback 的之上的。callback 就是回调。重点说 Future，
+             *  它的核心思想是：假设一个方法 fun，计算过程可能非常耗时，等待 fun 返回显然不合适。那么可以在调用 fun 的时候，立马返回一个 Future，
+             *  后续可以通过 Future 去监控方法 fun 的处理过程（即：Future-Listener 机制）
+             *
+             *
+             */
+            //ChannelFuture 表示异步的执行结果,可以通过它提供的方法来检测执行是否完成。
+            //ChannelFuture 是一个接口：public interface ChannelFuture extends Future<Void> 我们可以添加监听器，当监听的事件发生时，就会通知到监听器
+            //给cf 注册监听器，监控我们关心的事件
+            cf.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (cf.isSuccess()) {
+                        System.out.println("监听端口 6668 成功");
+                    } else {
+                        System.out.println("监听端口 6668 失败");
+                    }
+                }
+            });
+
+            cf.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+}
